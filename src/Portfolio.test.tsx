0b978e626eab888587ae85d692ee7a3f0bfbbaf6
Collin Argo <collinargo@gmail.com>
2019-05-23 11:14:13 -0400

    add router

--- src/Portfolio.test.tsx
+++ src/Portfolio.test.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as enzyme from "enzyme";
 import Portfolio from "./Portfolio";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 describe("Portfolio component", () => {
   it("renders without crashing", () => {
@@ -8,4 +10,10 @@ describe("Portfolio component", () => {
     ReactDOM.render(<Portfolio/>, div);
     ReactDOM.unmountComponentAtNode(div);
   });
+
+  it("has a router as a child", () => {
+    const portfolio = enzyme.shallow(<Portfolio/>);
+    expect(portfolio.find(Router).exists()).toBe(true);
+  });
+
 });
--- src/Portfolio.tsx
+++ src/Portfolio.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./Portfolio.css";
+import { BrowserRouter as Route, Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
 
 const Portfolio: React.FC = () => {
   return (
     <div className="Portfolio">
-
+      <Router history={createBrowserHistory()}>
+        <Route/>
+      </Router>
     </div>
   );
 };
