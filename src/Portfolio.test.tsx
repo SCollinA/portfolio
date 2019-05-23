51f55af86c46a6e2c2417ed36758512e33334c83
Collin Argo <collinargo@gmail.com>
2019-05-23 11:25:02 -0400

    add Home component

--- src/Portfolio.test.tsx
+++ src/Portfolio.test.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import * as enzyme from "enzyme";
 import Portfolio from "./Portfolio";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./Home/Home";
 
 describe("Portfolio component", () => {
   it("renders without crashing", () => {
@@ -16,4 +17,12 @@ describe("Portfolio component", () => {
     expect(portfolio.find(Router).exists()).toBe(true);
   });
 
+  it("renders Home component at '/' path", () => {
+    const portfolio = enzyme.shallow(<Portfolio/>);
+    const router = portfolio.find(Router);
+    expect(router.findWhere(a => {
+      return a.prop("path") === "/" &&
+        a.prop("component") === Home;
+      }).exists()).toBe(true);
+  });
 });
