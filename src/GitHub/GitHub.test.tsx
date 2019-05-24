6bf53d02d3c9b499308da61e66b7eec0222c3b59
Collin Argo <collinargo@gmail.com>
2019-05-24 09:13:15 -0400

    display repo names pulled from GitHub API

--- src/GitHub/GitHub.test.tsx
+++ src/GitHub/GitHub.test.tsx
@@ -1,6 +1,8 @@
 import * as enzyme from "enzyme";
 import React from "react";
-import GitHub from "./GitHub";
+import { Query } from "react-apollo";
+import { gitHubClient } from "../apollo/client";
+import GitHub, { gitHubRepos, INode, REPO_NAMES } from "./GitHub";
 
 describe("GitHub component", () => {
     it("renders without crashing", () => {
@@ -8,9 +10,34 @@ describe("GitHub component", () => {
         expect(github.exists()).toBe(true);
     });
 
-    it("renders an h1 as only child", () => {
+    // it("renders an h1 as only child", () => {
+    //     const github = enzyme.shallow(<GitHub/>);
+    //     expect(github.getElements().length).toBe(1);
+    //     expect(github.childAt(0).is("h1")).toBe(true);
+    // });
+
+    it("calls GitHub API and queries for repos", () => {
         const github = enzyme.shallow(<GitHub/>);
-        expect(github.getElements().length).toBe(1);
-        expect(github.childAt(0).is("h1")).toBe(true);
+        expect(github.find(Query).exists()).toBe(true);
+        expect(github.find(Query).prop("query")).toBe(REPO_NAMES);
+    });
+
+    it("destructures data only after it has loaded", () => {
+        expect(gitHubRepos).not.toThrowError();
+    });
+
+    it("displays names of repos as h1's", (done) => {
+        gitHubClient.query({
+            query: REPO_NAMES,
+        })
+        .then(({ data, loading }) => {
+            if (!loading) {
+                const github = enzyme.shallow(<GitHub/>);
+                data.user.repositories.nodes.forEach((node: INode) => {
+                    expect(github.find("h1").text()).toBe(node.name);
+                });
+            }
+        });
+        done();
     });
 });
--- src/GitHub/GitHub.tsx
+++ src/GitHub/GitHub.tsx
@@ -1,9 +1,60 @@
+import gql from "graphql-tag";
 import React from "react";
+import { Query } from "react-apollo";
 
 export default () => {
     return (
         <div className="GitHub">
-            <h1>Hello GitHub!</h1>
+            <Query query={REPO_NAMES}>
+                {gitHubRepos}
+            </Query>
         </div>
     );
 };
+
+export const gitHubRepos = (response: IQueryResponse) => {
+    let nodes;
+    if (response && !response.loading) {
+        nodes = response.data.user.repositories.nodes;
+    }
+    return (
+        <div className="repositories">
+            {nodes && nodes.map((node, index) => <h1 key={index}>{node.name}</h1>)}
+        </div>
+    );
+};
+
+export const REPO_NAMES = gql`
+    query {
+        user(login: "SCollinA") {
+            name
+            repositories(first: 50) {
+                nodes {
+                    name
+                }
+            }
+        }
+    }
+`;
+
+export interface IQueryResponse {
+    data: IRepoData;
+    loading: boolean;
+}
+
+export interface IRepoData {
+    user: IUser;
+}
+
+export interface IUser {
+    name: string;
+    repositories: IRepo;
+}
+
+export interface IRepo {
+    nodes: INode[];
+}
+
+export interface INode {
+    name: string;
+}
