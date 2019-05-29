7de10ff3486ea0061e32759342fa76993a026fd5
Collin Argo <collinargo@gmail.com>
2019-05-29 13:53:10 -0400

    remove bluebird and just use redis callbacks

--- back-end/src/redis/client.test.ts
+++ back-end/src/redis/client.test.ts
@@ -2,6 +2,17 @@ import { client as redisClient } from "./client";
 
 describe("Redis client", () => {
     it("connects to in-memory db", () => {
-        expect(redisClient.server_info).toBeDefined();
+        expect(redisClient.connected).toBe(true);
+    });
+
+    it("clears all keys on connect", (done) => {
+        redisClient.scan("0", (err, reply) => {
+            expect(err).toBeFalsy();
+            // reply[0] is the cursor
+            expect(reply[0]).toBe("0");
+            // reply[1] is the array of keys
+            // expect(reply).toBeFalsy();
+            done();
+        });
     });
 });
