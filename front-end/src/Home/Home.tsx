257a6821d6dd846a71c7bd6839369d44c28e6267
Collin Argo <collinargo@gmail.com>
2019-06-03 15:09:46 -0400

    style for mobile

--- front-end/src/Home/Home.tsx
+++ front-end/src/Home/Home.tsx
@@ -6,7 +6,12 @@ export default () => {
         <div className="Home">
             <h1>Hello World!</h1>
             <img src="./images/profile.jpg" alt="Collin's profile"/>
-            <p>bio</p>
+            <p>
+                Welcome to the latest installment of Collin Argo's portfolio!
+                This version was built with some pretty cool things like GraphQL
+                and schema stitching. Please explore the site and check out the 
+                source code!
+            </p>
         </div>
     );
 };
