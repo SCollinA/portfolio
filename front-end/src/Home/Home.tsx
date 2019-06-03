f06e2c267ff0093688d43ace5e33ea644fa8e263
Collin Argo <collinargo@gmail.com>
2019-06-03 15:27:21 -0400

    give background image to all pages

--- front-end/src/Home/Home.tsx
+++ front-end/src/Home/Home.tsx
@@ -5,12 +5,17 @@ export default () => {
     return (
         <div className="Home">
             <h1>Hello World!</h1>
-            <img src="./images/profile.jpg" alt="Collin's profile"/>
+            <img
+                src="./images/profile.jpg"
+                alt="Collin's profile"
+            />
             <p>
-                Welcome to the latest installment of Collin Argo's portfolio!
-                This version was built with some pretty cool things like GraphQL
-                and schema stitching. Please explore the site and check out the 
-                source code!
+                Welcome to the latest installment of
+                Collin Argo's portfolio! This version
+                was built with some pretty cool things
+                like GraphQL and schema stitching.
+                Please explore the site and check out
+                the source code!
             </p>
         </div>
     );
