f93b368a3a8385f5db9331c9abb0cce9a4a17a9a
Collin Argo <collinargo@gmail.com>
2019-06-03 14:42:17 -0400

    style home page

--- front-end/src/Home/Home.test.tsx
+++ front-end/src/Home/Home.test.tsx
@@ -8,9 +8,14 @@ describe("Home component", () => {
         expect(home.exists()).toBe(true);
     });
 
-    it("renders an h1 as only child", () => {
+    it("renders three children: h1, img, p", () => {
         const home = enzyme.shallow(<Home/>);
-        expect(home.getElements().length).toBe(1);
-        expect(home.find("h1").exists()).toBe(true);
+        expect(home.children().length).toBe(3);
+    });
+
+    it("renders an h1 with text 'Hello World!' as first child", () => {
+        const home = enzyme.shallow(<Home/>);
+        expect(home.childAt(0).is("h1")).toBe(true);
+        expect(home.childAt(0).text()).toBe("Hello World!");
     });
 });
--- front-end/src/Home/Home.tsx
+++ front-end/src/Home/Home.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import "./Home.css";
 
 export default () => {
     return (
         <div className="Home">
             <h1>Hello World!</h1>
+            <img src="./images/profile.jpg" alt="Collin's profile"/>
+            <p>bio</p>
         </div>
     );
 };
