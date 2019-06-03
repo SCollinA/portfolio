200cbf11ca681b953f3c0ce384d6af89bb894f91
Collin Argo <collinargo@gmail.com>
2019-06-03 12:44:49 -0400

    add footer component and fix position of navbar and footer

---
+++ front-end/src/Footer/Footer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css";
+
+export default () => {
+    return (
+        <div className="Footer">
+            <div className="built-with">
+                <h4>Apollo</h4>
+            </div>
+            <div className="social-links">
+                GitHub
+            </div>
+            <div className="copy-date">
+                2019
+            </div>
+        </div>
+    );
+};
--- front-end/src/Portfolio.tsx
+++ front-end/src/Portfolio.tsx
@@ -3,6 +3,7 @@ import { ApolloProvider } from "react-apollo";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { client } from "./apollo/client";
 import Contact from "./Contact/Contact";
+import Footer from "./Footer/Footer";
 import GitHub from "./GitHub/GitHub";
 import Home from "./Home/Home";
 import NavBar from "./NavBar/NavBar";
@@ -19,6 +20,7 @@ const Portfolio: React.FC = () => {
           <Route exact={true} path="/github" component={GitHub}/>
           <Route exact={true} path="/resume" component={Resume}/>
           <Route exact={true} path="/contact" component={Contact}/>
+          <Route path="/" component={Footer}/>
         </Router>
       </ApolloProvider>
     </div>
