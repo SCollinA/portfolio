41e12bc9c5b8ebf536993be3bbf4e5d99810cc5f
Collin Argo <collinargo@gmail.com>
2019-12-24 10:08:29 -0500

    set production url

--- front-end/src/apollo/client.ts
+++ front-end/src/apollo/client.ts
@@ -12,7 +12,8 @@ import { HttpLink } from "apollo-link-http";
 // import fetch from "isomorphic-fetch";
 
 const httpLink = new HttpLink({
-    uri: "http://localhost:4020/graphql",
+    // uri: "http://localhost:4020/graphql",
+    uri: "https://collinargo.com/graphql",
 });
 
   // Create a WebSocket link:
